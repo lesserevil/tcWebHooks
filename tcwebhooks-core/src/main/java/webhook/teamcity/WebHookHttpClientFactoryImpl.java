335d85e5c6c226d4685729516a716da33dbe8c53
Shawn Edwards <lesser.evil@gmail.com>
2020-01-07 13:00:59 -0600

    Allow all certs

--- tcwebhooks-core/src/main/java/webhook/teamcity/WebHookHttpClientFactoryImpl.java
+++ tcwebhooks-core/src/main/java/webhook/teamcity/WebHookHttpClientFactoryImpl.java
@@ -2,12 +2,18 @@ package webhook.teamcity;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.TrustAllStrategy;
 
 public class WebHookHttpClientFactoryImpl implements WebHookHttpClientFactory {
 	
 	@Override
 	public HttpClient getHttpClient(){
-		return HttpClients.createDefault();
+		return HttpClients..custom()
+            .setSSLContext(new SSLContextBuilder().loadTrustMaterial(null, TrustAllStrategy.INSTANCE).build())
+            .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+            .build();
 	}
 
 }
